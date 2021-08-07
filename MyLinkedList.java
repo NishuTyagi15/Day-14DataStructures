8ebffc7adca584a5c16a1cf28a48b269d73f9d56
Nishu Tyagi <tyaginishu7@gmail.com>
2021-08-07 18:29:25 +0530

    Refactored:Added an Element in the Last

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -5,24 +5,6 @@ import org.junit.Test;
 
 public class MyLinkedList {
 	@Test
-	public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
-		
-		MyNode<Integer> myFirstNode = new MyNode<>(70);
-		MyNode<Integer> mySecondNode = new MyNode<>(30);
-		MyNode<Integer> myThirdNode = new MyNode<>(56);
-		MyLinkedListTest myLinkedListTest = new MyLinkedListTest();
-		myLinkedListTest.add(myFirstNode);
-		myLinkedListTest.add(mySecondNode);
-		myLinkedListTest.add(myThirdNode);
-		myLinkedListTest.printMyNodes();
-		
-		boolean result = myLinkedListTest.head.equals(myThirdNode) && 
-				         myLinkedListTest.head.getNext().equals(mySecondNode) &&
-				         myLinkedListTest.tail.equals(myFirstNode);
-		Assert.assertTrue(result);
-					
-	}
-	
 	public void given3NumbersWhenAppendedShouldBeAddedToLast() {
 		
 		MyNode<Integer> myFirstNode = new MyNode<>(56);
@@ -40,5 +22,4 @@ public class MyLinkedList {
 		Assert.assertTrue(result);
 					
 	}
-
 }
